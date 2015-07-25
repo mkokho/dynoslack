a2b4b87d97bbc10ce0ad6fb081cde1cbdcbf7484
Mikhail Kokho <m.kokho@gmail.com>
2015-07-25 15:14:47 +1200

    build test

--- src/test/scala/ThreadOverflowTest.scala
+++ src/test/scala/ThreadOverflowTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.FunSuite
 import scala.concurrent.duration._
 import scala.concurrent.{Promise, Future, Await}
 import scala.util.Random
+import scala.util.logging.Logged
 
 /**
  * @author: Mikhail Kokho
@@ -19,39 +20,45 @@ class ThreadOverflowTest extends FunSuite{
   }
   
   def threadOverflow(size: Int) {
+    val blockTime = 4 seconds
+
+    println(s"Creating $size threads. Each thread blocks for $blockTime")
     val schedulers = 1 to size map {x =>
       Executors.newScheduledThreadPool(1)
     }
 
     def task(id: Int) = new Runnable {
       override def run(): Unit = {
-//        println(s"Thread $id has been created. It will block forever")
-        Thread.sleep(10000)
+        Thread.sleep(blockTime.toMillis)
       }
     }
-
 
     schedulers.zipWithIndex.foreach{case (sch, i) =>
       sch.schedule(task(i), 0, TimeUnit.MILLISECONDS)
     }
 
     try{
-      Await.ready(neverFuture, 4 seconds)
+      Await.ready(neverFuture, blockTime)
     }catch{
       case t: Throwable =>
-        println("Shutting down schedulers, which frees threads")
+        println(s"Shutting down schedulers and free threads")
         schedulers foreach {sch => sch.shutdown()}
     }
   }
 
-  test("Thread overflow. Size: 15"){
+  test("Creating 15 threads..."){
     threadOverflow(15)
   }
 
-/*  test("Thread overflow. Size: 1000"){
-    threadOverflow(1000)
+  test("15 threads has been stopped"){}
+
+  test("Creating 100 threads..."){
+    threadOverflow(100)
   }
 
+  test("100 threads has benn stopped"){}
+
+  /*
   test("Thread overflow. Size: 10000"){
     threadOverflow(10000)
   }
