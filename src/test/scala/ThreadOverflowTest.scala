438b01a6e6716e3edf1fa5a21306b1db25a829aa
Mikhail Kokho <m.kokho@gmail.com>
2015-07-25 14:17:41 +1200

    new tests

---
+++ src/test/scala/ThreadOverflowTest.scala
@@ -0,0 +1,62 @@
+import java.util.concurrent.{TimeUnit, Executors}
+
+import org.scalatest.FunSuite
+
+import scala.concurrent.duration._
+import scala.concurrent.{Promise, Future, Await}
+import scala.util.Random
+
+/**
+ * @author: Mikhail Kokho
+ * @date: 7/25/2015.
+ */
+class ThreadOverflowTest extends FunSuite{
+
+  def neverFuture[T]: Future[T] = {
+    val p: Promise[T] = Promise()
+    //we don't call failure or success because we don't want it be completed
+    p.future
+  }
+  
+  def threadOverflow(size: Int) {
+    val schedulers = 1 to size map {x =>
+      Executors.newScheduledThreadPool(1)
+    }
+
+    def task(id: Int) = new Runnable {
+      override def run(): Unit = {
+//        println(s"Thread $id has been created. It will block forever")
+        Thread.sleep(10000)
+      }
+    }
+
+
+    schedulers.zipWithIndex.foreach{case (sch, i) =>
+      sch.schedule(task(i), 0, TimeUnit.MILLISECONDS)
+    }
+
+    try{
+      Await.ready(neverFuture, 4 seconds)
+    }catch{
+      case t: Throwable =>
+        println("Shutting down schedulers, which frees threads")
+        schedulers foreach {sch => sch.shutdown()}
+    }
+  }
+
+  test("Thread overflow. Size: 100"){
+    threadOverflow(100)
+  }
+
+  test("Thread overflow. Size: 1000"){
+    threadOverflow(1000)
+  }
+
+  test("Thread overflow. Size: 10000"){
+    threadOverflow(10000)
+  }
+
+  test("Thread overflow. Size: 30000"){
+    threadOverflow(30000)
+  }
+}
